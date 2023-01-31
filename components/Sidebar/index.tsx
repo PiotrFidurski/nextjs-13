425f68304c084e9aca306eb2a0b61844537dad36
PiotrFidurski <crunor55@hotmail.com>
2023-01-31 17:50:11 +0100

    add Sidebar component

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from "./index.module.css";
+
+export function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <nav className={styles.nav}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/pricing">Pricing</Link>
+        </li>
+        <li>
+          <Link href="/newsletter">Newsletter</Link>
+        </li>
+      </nav>
+    </div>
+  );
+}

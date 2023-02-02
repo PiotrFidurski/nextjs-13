2b506650844346c61bdf50ca622bcda8f6034059
PiotrFidurski <crunor55@hotmail.com>
2023-02-02 19:57:40 +0100

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,3 @@
-import { getData } from "@/utils/getData";
-
-const API_URL = "https://baconipsum.com/api/?callback=?type=all-meat";
-
 export default async function AboutPage() {
-  const { data } = await getData(API_URL);
-
-  if (!data) return null;
-
-  return (
-    <div>
-      <h1>About</h1>
-      <div dangerouslySetInnerHTML={{ __html: data }} />
-    </div>
-  );
+  return <div></div>;
 }
